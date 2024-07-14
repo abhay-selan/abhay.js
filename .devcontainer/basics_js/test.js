947c4f0390b62459bd47330fe799cf9e86d5f755
abhay-selan <abhaysilwaniya2213@gmail.com>
2024-07-14 08:24:20 +0000

    datatypes

--- .devcontainer/basics_js/test.js
+++ .devcontainer/basics_js/test.js
@@ -1,1 +1,17 @@
-console.log("hallo abhay")
+const accountID = 143243
+let accountEmail = "abhay@gmain.com"
+var accountpaasword = "123456"
+accountcity = "jaipur"
+
+accountEmail = "selan@gmail.com"
+accountpaasword = "654321"
+accountcity = "sikar"
+
+//accountID = 2
+console.log(accountID);
+console.log(accountEmail);
+console.log(accountpaasword);
+console.log(accountcity);
+
+
+console.table([accountID, accountEmail, accountpaasword, accountcity])
